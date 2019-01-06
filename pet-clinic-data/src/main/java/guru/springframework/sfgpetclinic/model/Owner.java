47d8a8e3c2c5351def57a7afffa1842bad532348
John Juan <john.juan@sap.com>
2019-01-06 18:02:53 -0500

    Added Visit entity, updated Owner entity with pet relationship. Closes #26.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,5 +1,18 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
 
+	private static final long serialVersionUID = -5242598582332322150L;
+
+	private Set<Pet> pets;
+
+	public Set<Pet> getPets() {
+		return pets;
+	}
+
+	public void setPets(Set<Pet> pets) {
+		this.pets = pets;
+	}
 }
